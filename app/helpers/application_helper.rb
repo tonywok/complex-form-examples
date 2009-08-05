248d04d4672e7d82023aeca53de7b4b39e1bfca0
Ryan Bates <ryan@railscasts.com>
2009-08-05 12:09:43 -0700

    adding remove link instead of checkbox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def remove_child_link(name, f)
+    f.hidden_field(:_delete) + link_to_function(name, "remove_fields(this)")
+  end
+  
   def add_child_link(name, f, method)
     fields = new_child_fields(f, method)
     link_to_function(name, h("insert_fields(this, \"#{method}\", \"#{escape_javascript(fields)}\")"))
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,3 +7,12 @@ function insert_fields(link, method, content) {
     before: content.replace(regexp, new_id)
   });
 }
+
+function remove_fields(link) {
+  var hidden_field = $(link).previous("input[type=hidden]");
+  if (hidden_field) {
+    hidden_field.value = '1';
+  }
+  $(link).up(".fields").hide();
+}
+
