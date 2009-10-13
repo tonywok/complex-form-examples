3d36b80371b6b42053cd7b7ab81a208ec06d9133
Ryan Bates <ryan@railscasts.com>
2009-10-13 10:47:56 -0700

    ensuring add/remove link behavior only applies when inside of a form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  $('.add_child').click(function() {
+$(function() {
+  $('form a.add_child').click(function() {
     var assoc   = $(this).attr('data-association');
     var content = $('#' + assoc + '_fields_template').html();
-    var regexp  = new RegExp('new_' + assoc, 'g')
+    var regexp  = new RegExp('new_' + assoc, 'g');
     var new_id  = new Date().getTime();
         
     $(this).parent().before(content.replace(regexp, new_id));    
     return false;
   });
   
-  $('.remove_child').live('click', function() {
+  $('form a.remove_child').live('click', function() {
     var hidden_field = $(this).prev('input[type=hidden]')[0];
     if(hidden_field) {
       hidden_field.value = '1';
