76b2176b5c5358a50f6a1bd87b4ea707a6470fcd
Ryan Bates <ryan@railscasts.com>
2009-10-13 10:44:57 -0700

    using helper methods when generating nested fields instead of placing html in ruby strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def remove_child_link(name, f)
-    f.hidden_field(:_delete) + "<a href=\"javascript:void(0)\" class=\"remove_child\">#{name}</a>"
+    f.hidden_field(:_delete) + link_to(name, "javascript:void(0)", :class => "remove_child")
   end
   
   def add_child_link(name, association)
-    "<a href=\"javascript:void(0)\" class=\"add_child\" data-association=\"#{association.to_s}\">#{name}</a>"
+    link_to(name, "javascript:void(0)", :class => "add_child", :"data-association" => association)
   end
   
   def new_child_fields_template(form_builder, association, options = {})
@@ -13,11 +13,10 @@ module ApplicationHelper
     options[:partial] ||= association.to_s.singularize
     options[:form_builder_local] ||= :f
     
-    ret = "<div id=\"#{association}_fields_template\" style=\"display: none\">"
-    form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
-      ret += render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+    content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
+      form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
+        render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+      end
     end
-    ret += "</div>"
-    ret
   end
 end
