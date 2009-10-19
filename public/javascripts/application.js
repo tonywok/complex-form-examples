e3be76416a1943814ead3721e73f1036f86fde0d
Tim Riley <tim@openmonkey.com>
2009-10-19 16:11:27 +1100

    JS to insert the appropriate ID for each of the associations in the deep heirarchy. Now the deeply nested form works.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,29 @@
 $(function() {
   $('form a.add_child').live('click', function() {
-    var assoc   = $(this).attr('data-association');
-    var content = $('#' + assoc + '_fields_template').html();
+    // Setup
+    var assoc   = $(this).attr('data-association');           // Name of child
+    var content = $('#' + assoc + '_fields_template').html(); // Fields template
+    
+    // Make the context correct by replacing new_<parents> with the generated ID
+    // of each of the parent objects
+    var context = ($(this).parents('.fields').children('input:first').attr('name') || '').replace(new RegExp('\[[a-z]+\]$'), '');
+    if(context) {
+      var parent_names = context.match(/[a-z]+_attributes/g) || []
+      var parent_ids   = context.match(/[0-9]+/g)
+      
+      for(i = 0; i < parent_names.length; i++) {
+        if(parent_ids[i]) {
+          content = content.replace(new RegExp('new_' + parent_names[i].replace('_attributes', ''), 'g'), parent_ids[i]);
+        }
+      }
+    }
+    
+    // Make a unique ID for the new child 
     var regexp  = new RegExp('new_' + assoc, 'g');
     var new_id  = new Date().getTime();
+    content     = content.replace(regexp, new_id)
         
-    $(this).parent().before(content.replace(regexp, new_id));    
+    $(this).parent().before(content);
     return false;
   });
   
