04c91fe705176038b609d4dfe1934f271225b544
Tim Riley <tim@openmonkey.com>
2009-10-19 11:02:48 +1100

    Use content_for to catch the output of the new_child_fields_template helper. Only allow the content to be captured once. In the view, render all the child field template outside of the form using named yields.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,14 +9,21 @@ module ApplicationHelper
   end
   
   def new_child_fields_template(form_builder, association, options = {})
-    options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
-    options[:partial] ||= association.to_s.singularize
-    options[:form_builder_local] ||= :f
+    content_for "#{association}_fields_template" do
+      options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
+      options[:partial] ||= association.to_s.singularize
+      options[:form_builder_local] ||= :f
     
-    content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
-      form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
-        render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+      content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
+        form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
+          render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
+        end
       end
-    end
+    end unless content_given?("#{association}_fields_template")
+  end
+  
+  def content_given?(name)
+    content = instance_variable_get("@content_for_#{name}")
+    ! content.nil?
   end
 end
